850444cf0a07572dbad5ccc7f3884609d15d00ee
aqshing <asm.best@email>
2020-10-21 22:18:17 +0800

    vim config script

--- configure.sh
+++ configure.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+##/bin/bash
 ###################################################
 # Filename: configure.sh
 # Author: shing
 # E-mail: www.asm.best
 # Created Time: Fri 09 Oct 2020 07:18:13 PM CST
 ###################################################
+
+set -xeuo pipefail
 
 ##cd ~
 ##echo -n "your home: "
@@ -19,28 +21,25 @@ cp -r -a ./vim ~/.vim
 if [ -d "~/.config" ]; then
 	mkdir ~/.config
 fi
-cp -r -a ./config/ ~/.config
+cp -r -a ./config/* ~/.config
 
-if [ -d "~/optapp/neovim/bin" ]; then
-	mkdir -p ~/optapp/neovim/bin
-fi
-cp -r ./nvim.appimage ~/optapp/neovim/bin
-cd ~/optapp/neovim/bin
-chmod u+x nvim.appimage
-ln -s nvim.appimage nvim
-ln -s nvim.appimage vi
-cd -
+##if [ -d "~/optappneovim/bin" ]; then
+##	echo "路径不存在"
+##	mkdir -p ~/optapp/neovim/bin
+##else
+##	echo "路径存在"
+##fi
+##cp ./nvim.appimage ~/optapp/neovim/bin
+##cd ~/optapp/neovim/bin
+##chmod u+x nvim.appimage
+##ln -s nvim.appimage nvim
+##ln -s nvim.appimage vi
+##cd -
 
 if [ -e "~/local/share/nvim/site/autoload/plug.vim" ]; then
 	cp ./plug.vim ~/local/share/nvim/site/autoload/plug.vim
 fi
 
 
-source ~/.bashrc
-vi +PlugIntsall
-
-
-
-
-
-
+##source ~/.bashrc
+##vi +PlugInstall
