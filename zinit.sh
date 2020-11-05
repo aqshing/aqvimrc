48a0e14aa9b73a90893d50d6306b898d4abb27e2
aqshing <asm.best@email>
2020-11-05 21:08:14 +0800

    my zshrc

---
+++ zinit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+###################################################
+# Filename: zinit.sh
+# Author: shing
+# Email: www.asm.best
+# Created: Thu Nov  5 20:53:24 2020
+###################################################
+
+set -xeuo pipefail
+
+if [ -d "~/.zinit" ]; then
+	mkdir ~/.zinit
+fi
+
+git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
+
+if [ -f "~/.zshrc" ]; then
+	cp ./zshrc ~/.zshrc
+else
+	echo "please add to your [~/.zshrc]"
+	cat ./zshrc
+fi
---
+++ zshrc
@@ -0,0 +1,3 @@
+source ~/.zinit/bin/zinit.zsh
+source ~/.config/zsh/zinit.zshrc
+source ~/.config/zsh/share.shrc
